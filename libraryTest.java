ac6015b2705c2705d57c5f3dfb2767973d823abc
muskanpandita <159165927+muskanpandita@users.noreply.github.com>
2024-08-11 01:36:33 +0530

    Update libraryTest.java

--- libraryTest.java
+++ libraryTest.java
@@ -1,13 +1,13 @@
 package repo;
-
-public class libraryTest {
-    library library;
-    Book book;
+import java.util.*;
+public class LibraryTest {
+    Library library;
+    Book book; //book is used
 
     public void add(){
-        library= new library();
+        library= new Library();
         //isbn, title, author, Pyear
-        book=new Book("978-8171673400","Godaan","Munshi Premchand","1936");
+        book=new Book("978-8171673400","Godaan","Munshi Premchand",1936);
     }
 
     public void test_add_book(){
