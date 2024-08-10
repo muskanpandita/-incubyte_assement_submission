3d8b8d320449e25701bfe55a444ab1b08a2dcc6b
muskanpandita <159165927+muskanpandita@users.noreply.github.com>
2024-08-11 01:37:47 +0530

    Add files via upload

---
+++ MainFile.java
@@ -0,0 +1,11 @@
+package repo;
+
+public class MainFile {
+    public static void main(String args[]){
+        LibraryTest test=new LibraryTest();
+        test.add();
+        test.test_add_book();
+        System.out.println("Test passed");
+
+    }
+}
