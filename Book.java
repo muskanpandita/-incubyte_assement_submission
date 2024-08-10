33a362500bdc85acc040744a5d245d8909ba3b64
muskanpandita <159165927+muskanpandita@users.noreply.github.com>
2024-08-11 02:20:29 +0530

    Update Book.java

--- Book.java
+++ Book.java
@@ -6,12 +6,14 @@ public class Book {
     String title;
     String author;
     int publicationYear;
+    boolean borrowBooks;
 
 public Book(String ISBN, String title, String author, int publicationYear){
     this.ISBN=ISBN;
     this.title=title;
     this.author=author;
     this.publicationYear=publicationYear;
+    this.borrowBooks=false; 
 }
 
 public String getISBN(){
@@ -28,6 +30,14 @@ public String getAuthor(){
 
 public int getPublicationYear(){
     return publicationYear;
+}
+
+public boolean borrowBooks(){
+    return borrowBooks;
+}
+
+public void setBorrowed(boolean Borrowed){
+    borrowBooks= Borrowed;
 }
 
 }
